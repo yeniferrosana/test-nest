4a75afcdb603e9d9a0c01b6c5535f953421d5353
Daniel <dyangeltk17@gmail.com>
2023-02-12 01:29:04 -0500

    feat: add new components

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+### Documentacion del proyecto
+
+Primero clonar el proyecto
+
+Para la creación de componentes se deberá esta nomenclatura:
+
+`Crear el archivo CWHeader.vue`
+
+Donde `CW viene de Crowfunding` solo para tener una convención en nuestros componentes
---
+++ frontend/src/views/CFDetailView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import CFDetailCard from "../components/Cards/CFDetailCard.vue";
+import CFText from "../components/Text/CFText.vue";
+import arrowLeftIcon from "../assets/icons/arrow_left.svg";
+</script>
+<template>
+  <img :src="arrowLeftIcon" alt="">
+  <CFText tag="h3">Proyecto Don Tata</CFText>
+  <CFDetailCard />
+</template>
