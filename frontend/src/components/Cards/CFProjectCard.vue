95150da8249dae8822a0bcc9734525b6457654e3
Mayder C <maydercortes1@gmail.com>
2023-02-14 00:59:39 -0600

    feat: add projects page

---
+++ frontend/src/components/Cards/CFProjectCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "CFProjectCard",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/CFProjectsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div role="main" class="bg-secondary-color min-h-screen">
+    <header class="h-72 flex items-center justify-center relative">
+      <router-link to="/" class="arrow-container relative right-[10%]">
+        <img src="../assets/img/back-arrow.svg" alt="" />
+      </router-link>
+      <h1
+        class="font-montserrat text-3xl font-semibold text-primary-color whitespace-nowrap"
+      >
+        Explorá todos los proyectos
+      </h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CFProjectsView",
+};
+</script>
+
+<style lang="scss" scoped></style>
