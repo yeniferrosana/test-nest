c5b2d84b7dd40df7b60c3fccb106cf7354942182
Yohan Olmedo <yohancasaol@gmail.com>
2023-02-14 21:54:09 -0500

    Se agrega parcialmente los recursos para autenticar, incluyendo la entidad User

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfiguration } from './config/app.config';
 import { JoiValidationSchema } from './config/joi.validation';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +14,17 @@ import { JoiValidationSchema } from './config/joi.validation';
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/auth/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  first_name: string;
+
+  @Column('text')
+  last_name: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  user_name: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text', {
+    select: false,
+  })
+  password: string;
+
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+}
