809e88ba80062eb57b38d2913ee399f5e2ea0603
Yohan Olmedo <yohancasaol@gmail.com>
2023-02-23 14:32:32 -0500

    Se implementa lógica para la entidad organization

--- backend/src/organization/organization.controller.ts
+++ backend/src/organization/organization.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Patch,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { GetUser } from 'src/auth/decorators';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { User } from 'src/auth/entities/user.entity';
+import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { OrganizationService } from './organization.service';
+import { ValidRoles } from '../auth/interfaces/valid-roles.interface';
+import { UpdateOrganizationDto } from './dto/update-organization.dto';
 
 @Controller('organization')
 export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
+  @Post('register')
+  @Auth()
+  create(
+    @Body() createOrganizationDto: CreateOrganizationDto,
+    @GetUser() user: User,
+  ) {
+    return this.organizationService.create(createOrganizationDto, user);
+  }
+
+  @Get()
+  @Auth(ValidRoles.admin)
+  findAll(@Query('registered') registered: boolean) {
+    return this.organizationService.findAll(registered);
+  }
+
+  @Get(':id')
+  @Auth(ValidRoles.admin)
+  findOne(@Param('id') id: string) {
+    console.log('ss');
+    return this.organizationService.findOne(id);
+  }
+
+  @Patch(':id')
+  @Auth()
+  update(
+    @Param('id') id: string,
+    @Body() updateOrganizationDto: UpdateOrganizationDto,
+    @GetUser() user: User,
+  ) {
+    return this.organizationService.update(id, updateOrganizationDto, user);
+  }
 }
--- backend/src/organization/organization.module.ts
+++ backend/src/organization/organization.module.ts
@@ -3,10 +3,11 @@ import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
 import { Organization } from './entities/organization.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [OrganizationController],
   providers: [OrganizationService],
-  imports: [TypeOrmModule.forFeature([Organization])],
+  imports: [TypeOrmModule.forFeature([Organization]), AuthModule],
 })
 export class OrganizationModule {}
