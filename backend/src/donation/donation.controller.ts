01fe9c723a444e7905d48be13d775faa036898fe
Pablo Ortega <p.ortegariera@gmail.com>
2023-03-01 08:52:29 -0300

    feat: MercadoPago integration

--- backend/src/donation/donation.controller.ts
+++ backend/src/donation/donation.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DonationService } from './donation.service';
+import { PaymentData } from './dto/paymentData.dto';
+import { MercadoPagoService } from './mercadoPago.service';
 
 @Controller('donation')
 export class DonationController {
-  constructor(private readonly donationService: DonationService) {}
+  constructor(
+    private readonly donationService: DonationService,
+    private readonly mercadoPagoService: MercadoPagoService,
+  ) {}
+
+  @Post('mp')
+  async payment(@Body() body: PaymentData): Promise<any> {
+    const initPoint = await this.mercadoPagoService.payment(body);
+    return { initPoint };
+  }
 }
--- backend/src/donation/donation.module.ts
+++ backend/src/donation/donation.module.ts
@@ -3,10 +3,11 @@ import { DonationService } from './donation.service';
 import { DonationController } from './donation.controller';
 import { Donation } from './entities/donation.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MercadoPagoService } from './mercadoPago.service';
 
 @Module({
   controllers: [DonationController],
-  providers: [DonationService],
+  providers: [DonationService, MercadoPagoService],
   imports: [TypeOrmModule.forFeature([Donation])],
 })
 export class DonationModule {}
